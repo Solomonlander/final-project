18ea471147085fd0ef7d25076822d869df84cd45
Landeremmanson <solomonemmanson491@gmail.com>
2026-04-13 21:30:27 +0100

    Added product display feature

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-document.getElementById("app").innerHTML = "Project is working!";
+fetch("data/data.json")
+    .then(response => response.json())
+    .then(data => {
+        const app = document.getElementById("app");
+
+        data.forEach(item => {
+            const div = document.createElement("div");
+
+            div.innerHTML = `
+        <h3>${item.name}</h3>
+        <p>Price: $${item.price}</p>
+        <hr/>
+      `;
+
+            app.appendChild(div);
+        });
+    });
