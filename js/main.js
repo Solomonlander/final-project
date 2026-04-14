b0e2b6d7e977a44fece6a01ff3ca855ae4322d26
Landeremmanson <solomonemmanson491@gmail.com>
2026-04-14 10:03:10 +0100

    Remove old text and display products

--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@ fetch("data/data.json")
     .then(data => {
         const app = document.getElementById("app");
 
+        app.innerHTML = ""; // clear old text
+
         data.forEach(item => {
             const div = document.createElement("div");
 
             div.innerHTML = `
         <h3>${item.name}</h3>
         <p>Price: $${item.price}</p>
-        <hr/>
       `;
 
             app.appendChild(div);
